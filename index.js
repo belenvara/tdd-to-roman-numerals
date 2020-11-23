b8513a20e69b6a7f4d88ed4ddc80fb75b6a23686
Aula <Aula@EQUIP079588.xunta.local>
2020-11-23 13:49:11 +0100

    numeros romanos hasta 1000

--- index.js
+++ index.js
@@ -8,29 +8,55 @@
 export function toRoman(n) {
   // Refactorización
 let result = "" ;
-if (n==4){
-   result="IV" ;
-   n-=4;
+while (n>=1000){
+  result += "M";
+  n-=1000;
 }
-if (n==9){
-  result="IX"
-  n-=9;
+if (n>=900){
+  result += "CM";
+  n-=900;
 }
-else if (n>=10){
+if (n>=400){
+  result += "CD";
+  n-=400;
+}
+while (n>=100){
+  result += "C";
+  n-=100;
+}
+if (n>=90){
+  result+= "XC";
+  n-=90;
+}
+if (n>=50){
+  result+= "L";
+  n-=50;
+}
+if (n>=40){
+  result += "XL";
+  n-=40;
+}
+while (n>=10){
   result += "X";
   n-=10;
 }
-else if (n>=5){ 
+if (n==9){
+  result +="IX"
+  n-=9;
+}
+if (n==4){
+  result +="IV" ;
+  n-=4;
+}
+if (n>=5){ 
   result += "V";
   n-=5;
 
 }
 
-
 for (let i=0; i<n; i++){
   result +="I";
 }
-
 
 return result;
  // if (n==2) return"II";
--- test.js
+++ test.js
@@ -10,5 +10,22 @@ describe(('Conversión a números romanos'), () => {
   it('test 7', () => { strictEqual(toRoman(7), "VII") });
   it('test 9', () => { strictEqual(toRoman(9), "IX") });
   it('test 10', () => { strictEqual(toRoman(10), "X") });
+  it('test 11', () => { strictEqual(toRoman(11), "XI") });
+  it('test 14', () => { strictEqual(toRoman(14), "XIV") });
+  it('test 15', () => { strictEqual(toRoman(15), "XV") });
+  it('test 18', () => { strictEqual(toRoman(18), "XVIII") });
+  it('test 19', () => { strictEqual(toRoman(19), "XIX") });
+  it('test 20', () => { strictEqual(toRoman(20), "XX") });
+  it('test 35', () => { strictEqual(toRoman(35), "XXXV") });
+  it('test 40', () => { strictEqual(toRoman(40), "XL") });
+  it('test 45', () => { strictEqual(toRoman(45), "XLV") });
+  it('test 48', () => { strictEqual(toRoman(48), "XLVIII") });
+  it('test 49', () => { strictEqual(toRoman(49), "XLIX") });
+  it('test 54', () => { strictEqual(toRoman(54), "LIV") });
+  it('test 89', () => { strictEqual(toRoman(89), "LXXXIX") });
+  it('test 90', () => { strictEqual(toRoman(90), "XC") });
+  it('test 114', () => { strictEqual(toRoman(114), "CXIV") });
+  it('test 400', () => { strictEqual(toRoman(400), "CD") });
+  it('test 900', () => { strictEqual(toRoman(900), "CM") });
 })
 
