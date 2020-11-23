fbfd374e4e79fdbe1d8231f3ebd5b91a403b2015
Aula <Aula@EQUIP079588.xunta.local>
2020-11-23 11:45:58 +0100

    Numeros romanos hasta el 13

--- index.js
+++ index.js
@@ -6,7 +6,36 @@
  * @returns {string} El equivalente en números romanos.
  */
 export function toRoman(n) {
-  return "I";
+  // Refactorización
+let result = "" ;
+if (n==4){
+   result="IV" ;
+   n-=4;
+}
+if (n==9){
+  result="IX"
+  n-=9;
+}
+else if (n>=10){
+  result += "X";
+  n-=10;
+}
+else if (n>=5){ 
+  result += "V";
+  n-=5;
+
+}
+
+
+for (let i=0; i<n; i++){
+  result +="I";
+}
+
+
+return result;
+ // if (n==2) return"II";
+  //else if (n==3) return"III";
+ // else return "I";
 }
 
 
